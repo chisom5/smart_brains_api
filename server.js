a52aa1a019ace36a9f20d3f4fe0623511f350971
chisom5 <okoyechisom54@gmail.com>
2020-04-17 18:25:34 +0100

    adding database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "chisom05",
-    database: "smart_brain"
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
